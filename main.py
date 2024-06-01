7525ce210bf0c4de2d8012715085048f51d7b0f0
Alex Shem <as7322@ic.ac.uk>
2024-06-01 15:17:31 +0100

    Update .gitlab-ci.yml file

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
-from src.WebBrowser import WebBrowser
 
 if __name__ == "__main__":
     if "test" in sys.argv:
         print("Build verification only, skipping GUI initialisation")
     else:
         from PyQt5.QtWidgets import QApplication
+        from src.WebBrowser import WebBrowser
+
         app = QApplication(sys.argv)
         window = WebBrowser()
         window.show()
