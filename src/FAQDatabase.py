e4eccb68c00fb82df77b0ada5321131b1f374a83
Alex Slater <as4322@doc.ic.ac.uk>
2024-06-03 16:36:40 +0100

    [as4322] FAQDatabase.py: update database url and key to group supabase database, add domain to database if searched for first time

--- src/FAQDatabase.py
+++ src/FAQDatabase.py
@@ -2,21 +2,23 @@ import supabase
 
 
 class FAQDatabase:
-    DATABASE_URL = "https://sjkzlnubjyugjbcrhvdc.supabase.co"
-    DATABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqa3psbnVianl1Z2piY3Jodm" + \
-                   "RjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcxNTEwNDUsImV4cCI6MjAzMjcyNzA0NX0.wQgF7ybxI2RlwJ-d3Jaf8QEqg" + \
-                   "hYMdNrUVfggSb17CXI"
+    DATABASE_URL = "https://ptwlhvyvvukfvucjqfok.supabase.co"
+    DATABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZi" + \
+                   "I6InB0d2xodnl2dnVrZnZ1Y2pxZm9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc0M" + \
+                   "jQxNTUsImV4cCI6MjAzMzAwMDE1NX0.PqzDZk2H_YlNrkQ5c2FkDjIq7bEBtN2JIE0gfASzKUE"
 
     def __init__(self):
         self.supabase = supabase.create_client(self.DATABASE_URL, self.DATABASE_KEY)
 
     def getFAQ(self, domainName):
-        response = self.supabase.table("domains").select("dID").eq("domain", domainName).execute()
+        response = self.supabase.table("Domains").select("dID").eq("domain", domainName).execute()
 
         if response.data:
-            dID = response.data[0]["dID"]
-            response = self.supabase.table("questions").select("question").eq("dID", dID).execute()
-            return [r["question"] for r in response.data]
-        else:
             return []
-
+            # dID = response.data[0]["dID"]
+            # response = self.supabase.table("Questions").select("question").eq("dID", dID).order("references").execute()
+            # return [r["question"] for r in response.data]
+        else:
+            self.supabase.table("Domains").insert({"domain": domainName}).execute()
+            return []
+ 
